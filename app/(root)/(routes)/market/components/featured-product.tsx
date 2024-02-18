251ea68ae1141c04235f8fb90f0a5af251c46347
long <137044122+longleDevops@users.noreply.github.com>
2024-02-18 01:19:36 -0800

    feat: improve smooth product transitions

--- app/(root)/(routes)/market/components/featured-product.tsx
+++ app/(root)/(routes)/market/components/featured-product.tsx
@@ -14,13 +14,28 @@ interface FeaturedProductProps {
 
 export const FeaturedProduct: React.FC<FeaturedProductProps> = ({ ticker, companies }) => {
   const [scope, animate] = useAnimate();
+  const [scope2, animate2] = useAnimate();
+  const [isSwapped, setIsSwapped] = useState(true)
   useEffect(() => {
-    const timeout = setTimeout(() => {
-      animate(scope.current, { x: [150, 0], scale: [0, 1], opacity: [0.5, 1] }, { duration: .4 })
-    }, 2)
-    return () => clearTimeout(timeout)
+    //animate(scope.current, { opacity: 0.2 }, { duration: 0 })
+    if (isSwapped) {
+      animateFunc()
+    } else {
+      animateBackFunc()
+    }
+    setIsSwapped(!isSwapped)
   }, [ticker])
 
+  const animateFunc = () => {
+    animate(scope.current, { x: 150, scale: 0, opacity: 0 }, { duration: 0 })
+    animate2(scope2.current, { x: 0, scale: 1, opacity: 1 }, { duration: .4, ease: "easeInOut" })
+  }
+
+  const animateBackFunc = () => {
+    animate2(scope2.current, { x: 150, scale: 0, opacity: 0 }, { duration: 0 })
+    animate(scope.current, { x: 0, scale: 1, opacity: 1 }, { duration: .4, ease: "easeInOut" })
+
+  }
 
   const foundCompany = companies.find((company) => company.symbol === ticker)
   const companyName = foundCompany.yahooStockV2Summary.price.shortName
@@ -46,12 +61,21 @@ export const FeaturedProduct: React.FC<FeaturedProductProps> = ({ ticker, compan
       </div>
       <div className="relative flex items-center justify-center flex-1 ">
         <Image
-          className={"object-contain max-h-[300px]"}
+          className={"object-contain max-h-[300px] absolute"}
           src={`/products/${ticker.toLowerCase()}.webp`}
           alt="Product Image"
           width={300}
           height={300}
           ref={scope}
+          priority={true}
+        />
+        <Image
+          className={"object-contain max-h-[300px] absolute opacity-20 scale-[.2] translate-x-[150px] "}
+          src={`/products/${ticker.toLowerCase()}.webp`}
+          alt="Product Image"
+          width={300}
+          height={300}
+          ref={scope2}
           priority={true}
         />
       </div>
