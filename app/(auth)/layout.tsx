83dd9b7fa1ef061fbcca1badbfe5a60e844cfd96
HUONG MAI <maih@cwu.edu>
2024-03-02 22:45:50 -0800

    more companies

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-
+//This is a layout page for sign-in/sign-up page
 const layout = ({ children }: { children: React.ReactNode }) => {
   return (
     <div className="w-full h-screen flex items-center justify-center">
