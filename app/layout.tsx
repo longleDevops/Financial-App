09eac6ff0582ab2915e4d5b9a08d912f5e0e0515
long <137044122+longleDevops@users.noreply.github.com>
2024-03-06 23:13:20 -0800

    fix: disable default theming system to allow light mode

--- app/layout.tsx
+++ app/layout.tsx
@@ -32,7 +32,7 @@ export default function RootLayout({
             <ThemeProvider
               attribute="class"
               defaultTheme="light"
-              enableSystem
+              enableSystem={false}
               disableTransitionOnChange
             >
               {children}
