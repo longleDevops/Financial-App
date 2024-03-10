f801716d53981d88fd6c82a36b214e1faa3ba5d6
BBBB <81422101+KhangBBBB@users.noreply.github.com>
2024-03-09 17:52:27 -0800

    Update accordion-container

--- app/(root)/(routes)/dashboard/components/accordion-container.tsx
+++ app/(root)/(routes)/dashboard/components/accordion-container.tsx
@@ -1,3 +1,10 @@
+/**
+ * Component representing an Accordion container.
+ * 
+ * This component renders an Accordion component containing multiple AccordionItem components,
+ * each displaying different content based on user interaction.
+ */
+
 import {
   Accordion,
   AccordionContent,
@@ -10,26 +17,29 @@ import {
   Watchlist
 } from './index';
 
-
-
+/**
+ * Functional component representing an Accordion container.
+ * 
+ * @returns JSX.Element representing the Accordion container.
+ */
 export function AccordionContainer() {
-
   return (
     <Accordion type="multiple" className="w-full" defaultValue={["item-3"]}>
+      {/* AccordionItem for Portfolio. */}
       <AccordionItem value="item-1" className="px-4 ">
         <AccordionTrigger className="text-sm font-semibold">Porfolio</AccordionTrigger>
         <AccordionContent>
           <PortfolioItem />
         </AccordionContent>
       </AccordionItem>
-
+      {/* AccordionItem for Watchlist. */}
       <AccordionItem value="item-2" className="px-4">
         <AccordionTrigger className="text-sm font-semibold">Watchlist</AccordionTrigger>
         <AccordionContent>
           <Watchlist />
         </AccordionContent>
       </AccordionItem>
-
+      {/* AccordionItem for Distribution. */}
       <AccordionItem value="item-3" className="px-4 ">
         <AccordionTrigger className="text-sm font-semibold">Distribution</AccordionTrigger>
         <AccordionContent>
